010ef3d65f116fbd50ed3c8fda66f49482125071
zirwan salih <zirwanrose@gmail.com>
2023-10-09 21:30:13 -0400

    set up

--- src/test/java/stepdef/test1.java
+++ src/test/java/stepdef/test1.java
@@ -8,11 +8,13 @@ public class test1 {
     @Test
     public void login() {
         System.out.println("Logging In");
+        System.out.println("You are logged in!");
     }
 
 
     @Test
     public void logout() {
         System.out.println("Logging out");
+        System.out.println("Good Bye");
     }
 }
