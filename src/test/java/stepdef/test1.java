a20972c49ce4861a9dee2fdca61b5aad066522b3
zirwan salih <zirwanrose@gmail.com>
2023-10-09 21:23:09 -0400

    set up

---
+++ src/test/java/stepdef/test1.java
@@ -0,0 +1,18 @@
+package stepdef;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+
+    @Test
+    public void login() {
+        System.out.println("Logging In");
+    }
+
+
+    @Test
+    public void logout() {
+        System.out.println("Logging out");
+    }
+}
